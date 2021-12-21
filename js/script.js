1835bbe7f0b4024fbe1ef1e09dd776ea8e1a3e71
Boris <example@url.com>
2021-12-22 00:10:40 +0300

    added first interactive

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 "use strict";
 
-let number = 5;
-const leftBorderWidth = 1;
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "")
 
-console.log(leftBorderWidth);
 
-let incr = 10,
-    decr = 5;
+const personaMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
 
-incr++;
-decr--;
+const lastFilm = prompt("Один из последних просмотренных фильмов?", ""),
+    lastFilmRating = prompt("На сколько оцениваете его?", ""),
+    lastFilm2 = prompt("Один из последних просмотренных фильмов?", ""),
+    lastFilmRating2 = prompt("На сколько оцениваете его?", "");
+
+    personaMovieDB.movies[lastFilm] = lastFilmRating
+    personaMovieDB.movies[lastFilm2] = lastFilmRating2
+
+
+console.log(personaMovieDB.count, personaMovieDB.movies)
