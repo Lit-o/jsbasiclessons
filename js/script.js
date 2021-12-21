6e56ce3c83363c40af31f0c739b0a90ac2cb6610
Boris <example@url.com>
2021-12-22 01:54:49 +0300

    added for and if in script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "")
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "")
 
 const personaMovieDB = {
     count: numberOfFilms,
@@ -10,12 +10,28 @@ const personaMovieDB = {
     privat: false
 }
 
-// const lastFilm = prompt("Один из последних просмотренных фильмов?", ""),
-//     lastFilmRating = prompt("На сколько оцениваете его?", ""),
-//     lastFilm2 = prompt("Один из последних просмотренных фильмов?", ""),
-//     lastFilmRating2 = prompt("На сколько оцениваете его?", "");
+// for (let i = 0; i < 2; i++) {
+//     let lastFilm = prompt("Один из последних просмотренных фильмов?", ""),
+//     lastFilmRating = prompt("На сколько оцениваете его?", "");
 
-    personaMovieDB.movies[lastFilm] = lastFilmRating
-    personaMovieDB.movies[lastFilm2] = lastFilmRating2
+//     if (lastFilm !== null && lastFilmRating !== null && lastFilm !== "" && lastFilmRating !== "" && lastFilm < 50) {
+//         personaMovieDB.movies[lastFilm] = lastFilmRating 
+//         alert("done")
+//     } else {
+//         alert("error");
+//         i--;
+//     }    
+// }
+
+
+if (personaMovieDB.count < 10) {
+    alert("Мало кино")
+} else if (personaMovieDB.count >= 10 && personaMovieDB.count <= 30) {
+    alert("Классический зритель")
+} else if (personaMovieDB.count > 30) {
+    alert("Вы киноман")
+} else {
+    alert("error")
+}
 
 console.log(personaMovieDB.count, personaMovieDB.movies)
