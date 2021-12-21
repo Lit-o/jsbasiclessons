096c01a822a988237a6beb631c7db93e143d3eba
Boris <example@url.com>
2021-12-22 00:34:09 +0300

    added if block

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "")
 
-
 const personaMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -19,5 +18,35 @@ const lastFilm = prompt("Один из последних просмотренн
     personaMovieDB.movies[lastFilm] = lastFilmRating
     personaMovieDB.movies[lastFilm2] = lastFilmRating2
 
-
 console.log(personaMovieDB.count, personaMovieDB.movies)
+
+// // if block if block if block  if block if block if block
+// if (4 == 9) {
+//     console.log("Ok!")
+// } else {
+//     console.log("not Ok!")
+// }
+
+// const num = 50
+
+// if (num < 49) {
+//     console.log("not Ok!")
+// } else if (num > 100) {
+//     console.log("not Ok!")
+// } else {
+//     console.log("Ok!")
+// }
+
+// (num === 50) ? console.log("Ok!") : console.log("not Ok!")
+
+// switch (num) {
+//     case 49:
+//         console.log("not right!");
+//         break;
+//     case 50:
+//         console.log("Ok!")
+//         break;
+//     default:
+//         console.log("Ok!")
+//         break;
+// }
