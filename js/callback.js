a5275a26f848e4aba360e3d25ec6614b99757685
LitvinenkoBD <boris12lit@gmail.com>
2021-12-23 00:28:15 +0300

    added callback.js

---
+++ js/callback.js
@@ -0,0 +1,24 @@
+"use strict"
+
+function first() {
+    setTimeout(function() {
+        console.log(1)
+    }, 500)
+}
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+
+
+function learnJS(lang, callback) {
+    console.log(`Я учу ${lang}`);
+    callback();
+}
+
+learnJS("JS", function() {
+    console.log("Да, он действительно это учит");
+})
