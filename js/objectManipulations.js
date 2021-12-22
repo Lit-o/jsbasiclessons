f156ec935d6047f564eca15d4345c377315e2282
LitvinenkoBD <boris12lit@gmail.com>
2021-12-23 01:31:47 +0300

    added objectManipulation.js

---
+++ js/objectManipulations.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const obj = new Object();
+// старый синтаксис
+
+const options = {
+    name: "test",
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function () {
+        console.log("Test");
+    }
+}
+options.makeTest();
+delete options.name
+
+
+let counter = 0
+for (let key in options) {
+    if (typeof (options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`properties ${i} have ${options[key][i]}`);
+        }
+    } else {
+        console.log(`properties ${key} have ${options[key]}`);
+        counter++;
+    }
+}
+
+
+console.log(options['name'], counter)
+
+console.log(Object.keys(options).length)
