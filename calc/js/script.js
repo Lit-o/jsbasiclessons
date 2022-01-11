065253a07ca5d90d8a42a4431f4baa0024687fca
LitvinenkoBD <boris12lit@gmail.com>
2022-01-11 22:36:47 +0300

    added calc folder

---
+++ calc/js/script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const inputRub = document.querySelector('#rub');
+const inputUsd = document.querySelector('#usd');
+
+inputRub.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+
+    // request.open(method, url, async, login, pass);
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    // request.addEventListener('readystatechange', () => {
+    //     if (request.readyState === 4 && request.status === 200) {
+    //         console.log(request.response);
+    //         const data = JSON.parse(request.response);
+    //         inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2)
+    //     } else {
+    //         inputUsd.value = "Something wrong"
+    //     }
+    // })
+
+    request.addEventListener('load', () => {
+        if ( request.status === 200) {
+            console.log(request.response);
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2)
+        } else {
+            inputUsd.value = "Something wrong"
+        }
+    })
+
+    // список кодов состояния HTTP
+    // status
+    // statusText
+    // response
+    // readyState
+})
--- js/ajax.js
+++ js/ajax.js
@@ -2,9 +2,17 @@
 
 const persone = {
     name: "Alex'n",
-    tel: "+8213199900"
+    tel: "+8213199900",
+    parents: {
+        m: "Selesta",
+        d: "Bob"
+    }
 }
 
 console.log(JSON.stringify(persone));
-
 console.log(JSON.parse(JSON.stringify(persone)));
+
+// можно делать глубокую копию с помощью JSON
+const clone = JSON.parse(JSON.stringify(persone));
+
+
