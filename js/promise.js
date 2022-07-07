00809209ca65de5c395e4c31fae966cfb47c6f12
LitvinenkoBD <boris12lit@gmail.com>
2022-07-08 00:09:37 +0300

    extended comment to promise.js

--- js/promise.js
+++ js/promise.js
@@ -46,6 +46,7 @@ const test = time => {
 
 // test(1000).then(() => console.log('1000 ms'));
 // test(2000).then(() => console.log('2000 ms'));
-
+// Promise.all принимает массив промисов и выполняется когда все промисы выполнены
+// Promise.race принимает массив промисов и выполняется когда хотя бы один выполнен
 Promise.all([test(1000), test(2000)]).then(() => console.log('all'));
 Promise.race([test(1000), test(2000)]).then(() => console.log('all'));
