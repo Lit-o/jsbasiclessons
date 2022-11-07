6b9c980fa54192f274f15225947553ada96eeebf
LitvinenkoBD <boris12lit@gmail.com>
2022-11-07 19:18:08 +0300

    added boolToWord solution for AAA_codewars.js

--- js/AAA_codewars.js
+++ js/AAA_codewars.js
@@ -543,9 +543,14 @@ const doubleChar = s => {
   })
   return newArr.join('')
 } 
-
 // other solution
 const doubleChar2 = (str) => str.split("").map(c => c + c).join("");
-
 console.log(doubleChar('samfwq'))
 
+
+// --------------------------------------------------------------------------------------------------------------------
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+const boolToWord = b => b ? 'Yes' : 'No'
+
+
+// --------------------------------------------------------------------------------------------------------------------
