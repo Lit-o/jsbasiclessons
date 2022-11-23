aa3bffc3873c1322850b3a6e3a986182eaf9b258
Boris <boris12lit@gmail.com>
2022-11-23 21:52:57 +0300

    added new done task in AAA_codewars.js

--- js/AAA_codewars.js
+++ js/AAA_codewars.js
@@ -877,3 +877,19 @@ function SeriesSum(n) {
   return s.toFixed(2)
 }
 console.log(SeriesSum(2))
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// To be a senior, a member must be at least 55 years old and have a handicap greater than 7. 
+// In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+// input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+// output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+
+const openOrSenior = arr => arr.map(e => e[0] >= 55 && e[1] > 7 ? 'Senior' : 'Open')
+
+// other solution with destructure
+function openOrSenior(data){
+  return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+}
+
+console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]))
