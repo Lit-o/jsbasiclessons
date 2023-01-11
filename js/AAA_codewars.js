f219ab478ccc511b26172a921c54e36cf27b7707
Boris <boris12lit@gmail.com>
2023-01-11 18:15:09 +0300

    added marked cicle example

--- js/AAA_codewars.js
+++ js/AAA_codewars.js
@@ -985,3 +985,16 @@ const pineWood2 = (high) => {
 }
 
 console.log(pineWood2(4))
+
+
+// marked cicle
+first: for (let i = 0; i < 3; i++) {
+  console.log(`First lvl: ${i}`)
+  for (let j = 0; j < 3; j++) {
+    console.log(`Second lvl: ${j}`)
+    for (let k = 0; k < 3; k++) {
+      if (k === 2) continue first //or "break first" 
+      console.log(`Third lvl: ${k}`)      
+    }
+  }
+}
