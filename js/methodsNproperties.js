25f463c5394fcfa4a6b03117431be21ab06c461e
LitvinenkoBD <boris12lit@gmail.com>
2023-07-07 22:14:53 +0300

    added string methods and text cutter

--- js/methodsNproperties.js
+++ js/methodsNproperties.js
@@ -26,3 +26,29 @@ console.log(parseInt(numFromString));
 // с дробью
 console.log(parseFloat(numFromString));
 
+// Smart way to truncate long strings
+// есть 3 метода для получения подстроки: substring, substr и slice:
+
+// str.slice(start [, end]) Возвращает часть строки от start до (не включая) end.
+// Если аргумент end отсутствует, slice возвращает символы до конца строки:
+// Также для start/end можно задавать отрицательные значения. Это означает, 
+// что позиция определена как заданное количество символов с конца строки
+
+// str.substring(start [, end])
+// Возвращает часть строки между start и end (не включая) end.
+// Это — почти то же, что и slice, но можно задавать start больше end.
+// Если start больше end, то метод substring сработает так, как если бы аргументы были поменяны местами.
+// Отрицательные значения substring, в отличие от slice, не поддерживает, они интерпретируются как 0.
+
+// str.substr(start [, length])
+
+let string = "Ivan Ivaqnchov Braqga";
+// Обрезыватель текста, чего уж тут говорить...
+const textCutter = (text, cutPoint) => {
+  if (text.length > cutPoint) {
+    return text.slice(0, cutPoint-1) + '...'
+  }
+  return text
+}
+
+// subString.slice(0, subString.lastIndexOf(" "))
